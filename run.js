03282c1a718be56795024b7513992c9b6205cd83
harryb0905 <harryb0905@googlemail.com>
2018-02-14 21:40:02 +0000

    Can retrieve athlete data
    Able to retrieve basic athelete data based on client ID

--- run.js
+++ run.js
@@ -1,23 +1,9 @@
-var StravaApiV3 = require('strava_api_v3');
-var defaultClient = StravaApiV3.ApiClient.instance;
-
-// Configure OAuth2 access token for authorization: strava_oauth
-var strava_oauth = defaultClient.authentications['strava_oauth'];
-strava_oauth.accessToken = "YOUR ACCESS TOKEN"
-
-var api = new StravaApiV3.ActivitiesApi()
-
-var id = 789; // ***REMOVED***Long***REMOVED*** The identifier of the activity.
-
-var opts = ***REMOVED***
-  'includeAllEfforts': true // ***REMOVED***Boolean***REMOVED*** Whether all segments efforts should be included in the response
-***REMOVED***;
-
-var callback = function(error, data, response) ***REMOVED***
-  if (error) ***REMOVED***
-    console.error(error);
-  ***REMOVED*** else ***REMOVED***
-    console.log('API called successfully. Returned data: ' + data);
-  ***REMOVED***
-***REMOVED***;
-api.getActivityById(id, opts, callback);
+var strava = require('strava-v3');
+strava.athlete.get(***REMOVED***id:'CLIENT_ID'***REMOVED***, function(err,payload,limits) ***REMOVED***
+    if(!err) ***REMOVED***
+        console.log(payload);
+    ***REMOVED***
+    else ***REMOVED***
+        console.log(err);
+    ***REMOVED***
+***REMOVED***);
