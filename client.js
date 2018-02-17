dba4d2df569cdd55ba6ce3009808cb2de7379bf5
harryb0905 <harryb0905@googlemail.com>
2018-02-17 23:08:41 +0000

    Few head removed comments in code

--- client.js
+++ client.js
@@ -1,13 +1,19 @@
-// client.js
-// var socket = io.connect('http://localhost:3000/data', ***REMOVED***reconnect: true***REMOVED***);
-//
-// // Add a connect listener
-// socket.on('connect', function (socket) ***REMOVED***
-//     socket.send('Connected!');
-//     $('h1.connect-text').html("Connected!");
-// ***REMOVED***);
-var socket = io.connect('http://localhost:8010');
-socket.on('news', function (data) ***REMOVED***
-  console.log(data);
-  socket.emit('my other event', ***REMOVED*** my: 'data' ***REMOVED***);
-***REMOVED***);
+var socket = io.connect('http://127.0.0.1:4300');
+socket.on('connect', function(data) {
+  socket.emit('join', 'Hello World from client');
+});
+socket.on('messages', function(data) {
+   // Get first activity (testing)
+  var jsonArray = JSON.parse(data);
+  document.getElementById("athelete-info").innerHTML = JSON.stringify(data);
+
+   var count = 0;
+  jsonArray.forEach(function(activity) {
+     count++;
+  });
+  document.getElementById("activity-count").innerHTML = count;
+  document.getElementById("activity-type").innerHTML = jsonArray[0].type;
+
+  var totalDistance = jsonArray[0].distance;
+  document.getElementById("total-distance-text").innerHTML = totalDistance + "m (" + (totalDistance/1000).toFixed(2) + "km)";
+});
--- run.js
+++ run.js
@@ -6,7 +6,6 @@ var access_token = auth.access_token;
 var clientId = auth.client_id;
 
 var strava = require('strava-v3');
-<<<<<<< HEAD
 strava.athlete.get({access_token},function(err,payload,limits) {
     if(!err) {
         // console.log(payload);
@@ -20,27 +19,11 @@ function getActivityDetails(url, response) {
   https.get(url, (res) => {
     res.setEncoding('utf8');
     res.on('data', function (body) {
-=======
-strava.athlete.get(***REMOVED***access_token***REMOVED***,function(err,payload,limits) ***REMOVED***
-    if(!err) ***REMOVED***
-        // console.log(payload);
-    ***REMOVED***
-    else ***REMOVED***
-        console.log(err);
-    ***REMOVED***
-***REMOVED***);
-
-function getActivityDetails(url, response) ***REMOVED***
-  https.get(url, (res) => ***REMOVED***
-    res.setEncoding('utf8');
-    res.on('data', function (body) ***REMOVED***
->>>>>>> 179c6ee3f9d9b2a38984eaa48b988fff238b670f
         // Retrieves activity data
         var jsonData = JSON.parse(body);
         var jsonString = JSON.stringify(body);
         // console.log("Details = " + jsonString);
         return jsonString;
-<<<<<<< HEAD
     });
   });
 }
@@ -50,58 +33,29 @@ function getActivities(url, callback) {
   https.get(url, (res) => {
     res.setEncoding('utf8');
     res.on('data', function (body) {
-=======
-    ***REMOVED***);
-  ***REMOVED***);
-***REMOVED***
-
-// Retrieves all the activities for an authenticated athletes
-function getActivities(url, callback) ***REMOVED***
-  https.get(url, (res) => ***REMOVED***
-    res.setEncoding('utf8');
-    res.on('data', function (body) ***REMOVED***
->>>>>>> 179c6ee3f9d9b2a38984eaa48b988fff238b670f
         // Retrieves activity data
         var jsonData = JSON.parse(body);
         console.log("jsonData[0].id = " + jsonData[0].id);
         return callback(jsonData[0].id);
         // return jsonData[0].id;
-<<<<<<< HEAD
     });
   });
 }
-=======
-    ***REMOVED***);
-  ***REMOVED***);
-***REMOVED***
->>>>>>> 179c6ee3f9d9b2a38984eaa48b988fff238b670f
 
 // Currently only retrieves the first activity from the list
 const activitiesUrl = "https://www.strava.com/api/v3/athlete/activities?access_token="+access_token;
 var data;
 
-<<<<<<< HEAD
 getActivities(activitiesUrl, function(response) {
-=======
-getActivities(activitiesUrl, function(response) ***REMOVED***
->>>>>>> 179c6ee3f9d9b2a38984eaa48b988fff238b670f
     // Here you have access to your variable
     var activityId = response;
     const activityUrl = "https://www.strava.com/api/v3/activities/"+activityId+"?access_token="+access_token;
 
-<<<<<<< HEAD
     getActivityDetails(activityUrl, function(response) {
       var activityDetails = response;
       // console.log("Details = " + activityDetails);
     });
 });
-=======
-    getActivityDetails(activityUrl, function(response) ***REMOVED***
-      var activityDetails = response;
-      // console.log("Details = " + activityDetails);
-    ***REMOVED***);
-***REMOVED***);
->>>>>>> 179c6ee3f9d9b2a38984eaa48b988fff238b670f
 
 var express = require('express');
 var app = express();
@@ -110,7 +64,6 @@ var request = require('request');
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 
-<<<<<<< HEAD
 var path = require('path');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(__dirname + '/node_modules'));
@@ -141,33 +94,3 @@ app.get('/data', function(req, res){
     }
   });
 });
-=======
-app.use(express.static(__dirname + '/node_modules'));
-app.get('/', function(req, res,next) ***REMOVED***
-    res.sendFile(__dirname + '/index.html');
-***REMOVED***);
-
-io.on('connection', function(client) ***REMOVED***
-    console.log('Client connected...');
-    client.on('join', function(data) ***REMOVED***
-        request("https://www.strava.com/api/v3/athlete/activities?access_token="+access_token, function (error, response, body) ***REMOVED***
-            if (!error && response.statusCode == 200) ***REMOVED***
-              client.emit('messages', body);
-            ***REMOVED***
-        ***REMOVED***);
-    ***REMOVED***);
-***REMOVED***);
-server.listen(4200);
-
-// Display JSON in browser (127.0.0.1:4200/data)
-app.set('json spaces', 2);
-app.get('/data', function(req, res)***REMOVED***
-
-  request("https://www.strava.com/api/v3/athlete/activities?access_token="+access_token, function (error, response, body) ***REMOVED***
-    if (!error && response.statusCode == 200) ***REMOVED***
-      var activitiesJSON = JSON.parse(body);
-      res.json(activitiesJSON);
-    ***REMOVED***
-  ***REMOVED***);
-***REMOVED***);
->>>>>>> 179c6ee3f9d9b2a38984eaa48b988fff238b670f
