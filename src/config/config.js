53f33112ad3aa8cb86a4475d2e00b108e9409bbe
harryb0905 <harryb0905@googlemail.com>
2018-02-19 22:26:08 +0000

    Made config file generic

---
+++ src/config/config.js
@@ -0,0 +1,5 @@
+export const DEFAULT_URL = 'https://www.strava.com/api/v3/athletes/';
+export const ACTIVITIES_URL = 'https://www.strava.com/api/v3/athlete/activities';
+export const TOKEN_STR = 'access_token=';
+export const ACCESS_TOKEN = '<access token>';
+export const USER_ID = '<strava user id (as integer)';
