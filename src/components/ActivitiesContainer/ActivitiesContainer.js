4471d9586e4140ae9eb29816771b0daedbc11e41
harryb0905 <harryb0905@googlemail.com>
2018-02-26 20:56:50 +0000

    Added dates, more info and map controls

--- src/components/ActivitiesContainer/ActivitiesContainer.js
+++ src/components/ActivitiesContainer/ActivitiesContainer.js
@@ -17,37 +17,40 @@ export default class ActivitiesContainer extends Component {
   getActivities(url) {
     var actPromises = [];
     var self = this;
+    // Get all activities for authenticated athlete
     api.fetchStrava(url)
       .then((response) => {
+        // Push promise into array for further processing
         response.forEach(el => {
           actPromises.push(axios.get(`${ACTIVITIES_URL}/${el.id}?${TOKEN_STR}${ACCESS_TOKEN}`));
         });
       })
+      // Process all promises in array, retrieve detailed activity info
       .then((response) => {
         axios.all(actPromises).then(function(results) {
             var activities = [];
             results.forEach(function(response) {
-                console.log(response.data);
                 activities.push(response.data);
             })
-            console.log(activities);
             self.setState({activities: activities, loading: false});
         });
       });
   }
 
-  componentWillMount() {
+  componentDidMount() {
     this.getActivities(`${ACTIVITIES_URL}?${TOKEN_STR}${ACCESS_TOKEN}`);
   }
 
   render() {
     var activities = this.state.activities.map((r, i) => {
+      console.log(r);
       return (
         <Activity
           key={i}
           id={r.id}
           name={r.name}
           distance={r.distance}
+          totalTime={r.elapsed_time}
           maxSpeed={r.max_speed}
           avgSpeed={r.average_speed}
           description={r.description}
--- src/components/Activity/Activity.js
+++ src/components/Activity/Activity.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import './Activity.css';
-import ReactMapboxGl, { Layer, Feature } from "react-mapbox-gl";
+import ReactMapboxGl, { Layer, Feature, ZoomControl } from "react-mapbox-gl";
 import Card from 'material-ui/Card';
 import { MAPBOX_ACCESS_TOKEN } from '../../config/config';
 import runLogo from '../../Static/Run.gif';
 // import rideLogo from '../../Static/Ride.png';
 
 const Map = ReactMapboxGl({
-  accessToken: MAPBOX_ACCESS_TOKEN
+  accessToken: MAPBOX_ACCESS_TOKEN,
+  scrollZoom: false
 });
 
 var polyline = require('@mapbox/polyline');
@@ -46,7 +47,10 @@ export default class Activity extends Component {
     return (
       <Card style={{ margin: 50, padding: 55 }}>
         <div className="Activity">
-          <h3 className="title">{this.props.name}</h3>
+          <div className="topSection">
+            <h3 className="title">{this.props.name}</h3>
+            <p className="dateText">{this.props.date.split('T')[0]}   [{this.props.date.split('T')[1].split('Z')[0]}]</p>
+          </div>
           <p className="activityType">Type:
             <span><img src={runLogo} style={{width: 40, position: 'relative', top: 15, left: 10}} alt='Run'/></span>
           </p>
@@ -54,7 +58,6 @@ export default class Activity extends Component {
           <div className="mapContainer">
             <Map
               style="mapbox://styles/mapbox/streets-v9"
-              zoom={[12]}
               fitBounds={this.state.bounds}
               containerStyle={{
                 height: 450
@@ -68,16 +71,21 @@ export default class Activity extends Component {
                 }}>
                 <Feature coordinates={this.state.coords}/>
               </Layer>
+              <Layer
+                type="symbol"
+                id="marker"
+                layout={{ "icon-image": "marker-15" }}>
+                <Feature coordinates={this.state.coords[0]}/>
+              </Layer>
+              <ZoomControl/>
             </Map>
           </div>
           <div className="infoContainer">
             <p className="infoText">Information</p>
-            <p className="maxSpeed">Maximum Speed: <strong>{this.props.maxSpeed}m/s</strong></p>
-            <p className="avgSpeed">Average Speed: <strong>{this.props.avgSpeed}m/s</strong></p>
+            <p className="totalTime">Total Time: <strong>{(this.props.totalTime/60).toFixed(1)} mins</strong></p>
+            <p className="maxSpeed">Maximum Speed: <strong>{this.props.maxSpeed.toFixed(1)}m/s</strong></p>
+            <p className="avgSpeed">Average Speed: <strong>{this.props.avgSpeed.toFixed(1)}m/s</strong></p>
             <p className="descText">Description: <strong>{this.props.description ? this.props.description : '-'}</strong></p>
-          </div>
-          <div className="bottomDiv">
-            <p className="dateText">{this.props.date}</p>
           </div>
         </div>
       </Card>
